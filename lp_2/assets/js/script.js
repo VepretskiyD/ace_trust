48a90f15dee5c1622d7e532e1ec210ae24ad5d1c
Dmitriy Vepretskiy <d.vepretskiy@gmail.com>
2017-11-15 23:21:56 +0200

    Add check for 'navigator.vibrate' feature support

--- lp_2/assets/js/script.js
+++ lp_2/assets/js/script.js
@@ -1,5 +1,7 @@
-navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-navigator.vibrate([1000]);
+if ("vibrate" in navigator) {
+  navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+  navigator.vibrate([1000]);
+}
 
 function goto() {
   location.href = "http://you.goouturl.com/click"
